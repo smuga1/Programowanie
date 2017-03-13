2cd967911f81b98c0bfb066aaf49b6e58332e71a
kasia12345 <katarzyna.rzonca@hotmail.com>
2017-03-13 18:30:02 +0100

    Add files via upload

---
+++ Cwiczenia2/Zadanie1-Cw2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    
+    class AstonMartin : ISamochod
+    {
+        private string marka;
+        private string salon;
+
+        public AstonMartin(string marka, string salon)
+        {
+            this.marka = marka;
+            this.salon = salon;
+        }
+
+        public string WypiszMarke() { return marka; }
+        public string WypiszSalon() { return salon; }
+    }
+
+    class RangeRover : ISamochod
+    {
+        private string marka;
+        private string salon;
+
+        public RangeRover(string marka, string salon)
+        {
+            this.marka = marka;
+            this.salon = salon;
+        }
+        public string WypiszMarke() { return marka; }
+        public string WypiszSalon() { return salon; }
+    }
+
+    class RollsRoyce : ISamochod
+    {
+        private string marka;
+        private string salon;
+
+        public RollsRoyce(string marka, string salon)
+        {
+            this.marka = marka;
+            this.salon = salon;
+        }
+        public string WypiszMarke() { return marka; }
+        public string WypiszSalon() { return salon; }
+    }
+
+
+    public interface ISamochod
+    {
+        string WypiszMarke();
+        string WypiszSalon();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AstonMartin a1 = new AstonMartin("Aston Martin", "Salon1");
+            RangeRover a2 = new RangeRover("Range Rover", "Salon2");
+            RollsRoyce a3 = new RollsRoyce("Rolls Royce", "Salon 3");
+
+            a1.WypiszMarke();
+            a1.WypiszSalon();
+
+            a2.WypiszMarke();
+            a2.WypiszSalon();
+
+            a3.WypiszMarke();
+            a3.WypiszSalon();
+        }
+    }
+}
---
+++ Cwiczenia2/Zadanie3-Cw2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Osoba
+    {
+        protected string imie;
+        protected string nazwisko;
+        protected string dataUrodzenia;
+
+        public Osoba(string imie_, string nazwisko_, string dataUrodzenia_) { }
+        public void WypiszInfo() { }
+    }
+
+    class Student : Osoba
+    {
+        private int rok;
+        private int grupa;
+        private int nrIndeksu;
+
+        public Student(string imie_, string nazwisko_, string dataUrodzenia_, int rok, int grupa_, int nrIndeksu_) { }
+        public void WypiszInfo() { }
+        public void DodajOcene(string nazwaPrzedmiotu, string data, double wartosc) { }
+        public void WypiszOceny() { }
+        public void WypiszOceny(string nazwaPrzedmiotu) { }
+        public void UsunOcene(string nazwaPrzedmiotu, string data, double wartosc) { }
+        public void UsunOceny() { }
+        public void UsunOceny(string nazwaPrzedmiotu) { }
+    }
+
+    class Pilkarz : Osoba
+    {
+        private string pozycja;
+        private string klub;
+        private int liczbaGoli = 0;
+
+        public Pilkarz(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_) { }
+        public void WypiszInfo() { }
+        public void StrzelGola() { }
+    }
+
+    class PilkarzReczny : Pilkarz
+    {
+        public PilkarzReczny(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_) { }
+        public void StrzelGola() { }
+    }
+
+    class PilkarzNozny : Pilkarz
+    {
+        public PilkarzNozny(string imie_, string nazwisko_, string dataUrodzenia_, string pozycja_, string klub_) { }
+        public void StrzelGola() { }
+    }
+
+    class Ocena
+    {
+        private string nazwaPrzedmiotu;
+        private string data;
+        private double wartosc;
+
+        public Ocena(string nazwaPrzedmiotu_, string data_, double wartosc_) { }
+        public void WypiszInfo() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
